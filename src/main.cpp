131d9dcd47575efdc18c9645acba6590f8528c19
Brandon <32444660+bran-dev00@users.noreply.github.com>
2026-02-02 00:35:08 -0600

    imgui boilerplate window and updated path to audio samples

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,83 @@
 #include <Windows.h>
-#include <iostream>
 #include <mmsystem.h>
+
+#include <filesystem>
+#include <stdio.h>
+#include <iostream>
 #include <chrono>
-using namespace std::chrono_literals;
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl2.h"
+#include <GLFW/glfw3.h>
 
 int main(int argc, char const *argv[])
 {
-    
-    std::chrono::time_point<std::chrono::steady_clock> last_sound_timepoint = std::chrono::steady_clock::now();
 
-    while(true){
-        auto now = std::chrono::steady_clock::now();
+// imgui + glfw boilerplate
+    if (!glfwInit())
+        return 1;
 
-        if(std::chrono::duration_cast<std::chrono::milliseconds>(now - last_sound_timepoint) > 1000ms ){
-            PlaySound(TEXT("Snare.wav"), NULL, SND_FILENAME | SND_ASYNC);
-            last_sound_timepoint = now;
+    // Create window with OpenGL context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Drum Machine", NULL, NULL);
+    if (window == NULL)
+    {
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); 
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+
+    // Backend Bindings  
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL2_Init();
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL2_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            ImGui::Begin("Drum Machine");
+
+            //sample path
+            auto sampleWav = (std::filesystem::current_path() / L"assets" / L"Snare.wav").wstring();
+            if (ImGui::Button("Play Snare")) {
+                PlaySoundW(sampleWav.c_str(), NULL, SND_FILENAME | SND_ASYNC);
+            }
+            ImGui::End();
         }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
     }
 
-    return 0;
+    // Cleanup
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
